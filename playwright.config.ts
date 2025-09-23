328d9cb4308eb78163b1cddd1e4c190656e60ee0
veyronB2 <lokyt2@gmail.com>
2025-09-23 15:44:09 +0100

    updated playwright config to run on additional browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,30 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
     testDir: "src/tests/e2e",
     testMatch: "**/*.spec.ts",
     use: {
         trace: "off"
-    }
+    },
+    projects: [
+        {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] }
+        },
+        {
+            name: "firefox",
+            use: { ...devices["Desktop Firefox"] }
+        },
+        {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] }
+        },
+        {
+            name: "edge",
+            use: {
+                ...devices["Desktop Edge"],
+                channel: "msedge"
+            }
+        }
+    ]
 });
