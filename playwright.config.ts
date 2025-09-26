fbeedd3035e9092c1df94cb3aeb39b97e2da4e71
veyronB2 <lokyt2@gmail.com>
2025-09-26 09:10:28 +0100

    Updated playwright config to include webserver details. Also updated package json scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig, devices } from "@playwright/test";
 
+const baseUrl = "http://localhost:3000";
+
 export default defineConfig({
     testDir: "src/tests/e2e",
     testMatch: "**/*.spec.ts",
+    timeout: 60000,
+    fullyParallel: true,
+    webServer: {
+        command: "npm run dev",
+        url: baseUrl,
+        reuseExistingServer: !process.env.CI,
+        stdout: "ignore",
+        stderr: "pipe",
+    },
     use: {
         trace: "off"
     },
